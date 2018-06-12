a07cb825d74fffd14c013cc7068a3b00d2b22402
b0mbers <32966977+b0mbers@users.noreply.github.com>
2018-06-12 13:15:35 -0400

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,8 @@
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+          Console.WriteLine("Slope! -for the smart ones- ");
+          string ans = Console.ReadLine();
+        }
+    }
